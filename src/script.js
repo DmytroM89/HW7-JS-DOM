1a43712ef39cdb27fc4af8260258f323a7a76bb3
MartynenkoDV <MartynenkoDV@comfy.ua>
2022-04-09 21:45:49 +0300

    finish HW7-JS-DOM

---
+++ src/script.js
@@ -0,0 +1,39 @@
+const shape = document.querySelector('.shape');
+const shapeSelect = document.querySelector('#shapeSelect');
+const inputColor = document.querySelector('#inputColor');
+const applyColorBtn = document.querySelector('#applyColor');
+const resetBtn = document.querySelector('#reset');
+let color = '';
+let selectedShape = '';
+
+shapeSelect.addEventListener('change', (e) => {
+    selectedShape = e.target.value;
+    shape.innerText = '';
+    shape.className = selectedShape;
+    changeShapeColor();
+});
+
+applyColorBtn.addEventListener('click', () => {
+    color = inputColor.value;
+    changeShapeColor();
+});
+
+resetBtn.addEventListener('click', () => {
+    shape.innerText = 'shape';
+    shape.removeAttribute('style');
+    shape.className = 'shape';
+    shapeSelect.selectedIndex = 0;
+    inputColor.value = '#000000';
+    color = '';
+});
+
+function changeShapeColor() {
+    if (selectedShape === 'triangle') {
+        shape.style.background = 'transparent';
+        shape.style.borderBottomColor = color;
+    } else {
+        shape.style.background = color;
+        shape.style.borderBottomColor = '';
+    }
+}
+
